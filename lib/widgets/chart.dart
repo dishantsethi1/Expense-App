f45fa5e5dbd63762589434c06a42c27b53133e7c
Dishant Sethi <dishantsethi266@gmail.com>
2020-06-24 13:42:09 +0530

    added bars for transactions

--- lib/widgets/chart.dart
+++ lib/widgets/chart.dart
@@ -1,6 +1,7 @@
 import '../models/transaction.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import './chartbar.dart';
 
 class Chart extends StatelessWidget {
   final List<Transaction> recenttransactions;
@@ -17,27 +18,39 @@ class Chart extends StatelessWidget {
         if (recenttransactions[i].date.day == Weekday.day &&
             recenttransactions[i].date.month == Weekday.month &&
             recenttransactions[i].date.year == Weekday.year) {
-          totalsum = totalsum+recenttransactions[i].amount;
+          totalsum = totalsum + recenttransactions[i].amount;
         }
       }
       print(DateFormat.E().format(Weekday));
       print(totalsum);
 
       return {
-        'Day': DateFormat.E().format(Weekday),
+        'Day': DateFormat.E().format(Weekday).substring(0, 1),
         'amount': totalsum,
       };
     });
   }
 
+  double get maxspending {
+    return groupedtransactions.fold(0.0, (sum, item) {
+      return sum + item['amount'];
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-print(groupedtransactions);
+    print(groupedtransactions);
     return Card(
       elevation: 5,
       margin: EdgeInsets.all(20),
       child: Row(
-        children: <Widget>[],
+        children: groupedtransactions.map((data) {
+          return ChartBar(
+            data['Day'],
+            data['amount'],
+            maxspending==0.0?0.0:(data['amount'] as double) / maxspending,
+          );
+        }).toList(),
       ),
     );
   }
